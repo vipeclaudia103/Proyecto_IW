d9c6875e09e5e5241ddeafdc0749fff21f5cca87
Unai <unai.callleja@opendeusto.es>
2023-04-13 18:57:01 +0200

    Creación de los modelos

--- proyectoIWE2/appProyecto/models.py
+++ proyectoIWE2/appProyecto/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    cif = models.CharField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    telefono = models.IntegerField(max_length=20)
+    email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.cif, self.nombre, self.direccion, self.telefono, self.email
+
+class Pedido(models.Model):
+    cif = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    id_producto = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    fecha = models.DateTimeField('Fecha de creacion')
+    cantidad = models.IntegerField(max_length=1000)
+    precio = models.IntegerField(max_length=1000)
+
+class Producto(models.Model):
+    precio = models.IntegerField
+    nombre = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=1000)
+    categoria = models.CharField(max_length=200)
+
+class Componente(models.Model):
+    nombre = models.IntegerField(max_length=200)
+    marca = models.IntegerField(max_length=200)
+
+class Elemento(models.Model):
+    id_producto = models.IntegerField.ForeignKey(Producto, on_delete=models.CASCADE)
+    id_componente = models.IntegerField.ForeignKey(Componente, on_delete=models.CASCADE)
+    cantidad = models.IntegerField(max_length=1000)
