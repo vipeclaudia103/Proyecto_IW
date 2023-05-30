83b0720c8a5dcd1e5005281a6f3c6fdb2896f27d
Claudia V <vipeclaudia103@gmail.com>
2023-05-30 23:51:57 +0200

    ajax

---
+++ proyectoIWE2/appProyecto/static/js/ajaxFormularioComponente.js
@@ -0,0 +1,42 @@
+//ESTA EN emailCliente
+let formu = document.getElementById("formularioComponente");
+formu.addEventListener('submit', botonGuardar);
+
+async function botonGuardar(event) {
+    event.preventDefault();
+
+    const data = new FormData(formu);
+    response = await fetch("http://127.0.0.1:8000/appProyecto/componentes/", {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        body: data
+    }).then(function (response) {
+        if (response.ok) {
+            cuadroConfirmarAnadir();
+        } else {
+            throw "Error en la llamada Ajax";
+        }
+    });
+
+};
+async function cuadroConfirmarAnadir() {
+    let cuadroAnadir = document.createElement('div');
+    cuadroAnadir.className = 'anadirDiv';
+    cuadroAnadir.id = 'cuadroAnadirId';
+    let seleccionarSitio = document.getElementById("formularioComponentes");
+    cuadroAnadir.innerHTML = "Añadido correctamente";
+    seleccionarSitio.append(cuadroAnadir);
+    setTimeout(cuadroConfirmarQuitar, 3000);
+
+};
+
+function cuadroConfirmarQuitar() {
+    let selecciondivanadido = document.getElementsByClassName('anadirDiv');
+    for (var i = 0; i < selecciondivanadido.length; i++) {
+        selecciondivanadido[i].style.display = 'none';
+    }
+}
+
+
+
